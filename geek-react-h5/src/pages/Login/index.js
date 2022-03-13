8dae88cadc18520a6c95558f2ac21586b1887239
JuanYuan <kitty.yuan525@gmail.com>
2022-03-13 19:33:38 +1100

    Login function - Icon component encapsulation

--- geek-react-h5/src/pages/Login/index.js
+++ geek-react-h5/src/pages/Login/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Icon from "@/components/Icon"
+import "./index.scss"
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+      <Icon 
+        type="icon-renwu-01" 
+        className="big" 
+        onClick={() => console.log('clicked me')} 
+      />
+    </div>
   )
 }
