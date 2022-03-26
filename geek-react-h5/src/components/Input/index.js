23d880622ed84fd940eed62b20b528960ca1e97e
JuanYuan <kitty.yuan525@gmail.com>
2022-03-26 19:04:52 +1100

    Login function - Icon component added classNames

--- geek-react-h5/src/components/Input/index.js
+++ geek-react-h5/src/components/Input/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './index.module.scss'
+import classNames from 'classnames'
 
-export default function Input({extra, onExtraClick, ...rest}) {
+export default function Input({extra, onExtraClick, className, ...rest}) {
+    console.log('rest', rest)
   return (
     <div className={styles.root}>
-        <input className="input" {...rest} />
+        <input className={classNames("input", className)} {...rest} />
         {extra && <div className="extra" onClick={onExtraClick} >发送验证码</div>}
     </div>
   )
