9591ed869a74f06c9c7f50ce6f824d7733cb28c9
JuanYuan <kitty.yuan525@gmail.com>
2022-04-24 11:14:35 +1000

    Added Tabs static component

--- geek-react-h5/src/pages/Home/index.js
+++ geek-react-h5/src/pages/Home/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import styles from './index.module.scss'
+import Tabs from '@/components/Tabs'
 
 export default function Home() {
+  const tabs = [
+    {
+      id: 1,
+      name: '频道1',
+    },
+    {
+      id: 2,
+      name: '频道2',
+    },
+    {
+      id: 3,
+      name: '频道3',
+    },
+    {
+      id: 4,
+      name: '频道4',
+    },
+    {
+      id: 5,
+      name: '频道5',
+    },
+  ]
   return (
-    <div>首页</div>
+    <div className={styles.root}>
+      <Tabs tabs={tabs}></Tabs>
+    </div>
   )
 }
