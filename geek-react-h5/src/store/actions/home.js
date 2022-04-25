ee20dc9c583a008f915d9f2f32c14364e42a5583
JuanYuan <kitty.yuan525@gmail.com>
2022-04-25 11:03:07 +1000

    Home page - get and set channels info from 3 different conditions

--- geek-react-h5/src/store/actions/home.js
+++ geek-react-h5/src/store/actions/home.js
@@ -1,10 +1,25 @@
 import request from '@/utils/request';
+import { hasToken, getLocalChannels, setLocalChannels } from '@/utils/storage';
 import { SAVE_CHANNELS } from '../action_types/home'
 
 export const getUserChannels = () => {
     return async dispatch => {
-        const res = await request.get('/user/channels');
-        dispatch(saveUserChannels(res.data.channels))
+        if(hasToken) {
+            const res = await request.get('/user/channels');
+            dispatch(saveUserChannels(res.data.channels));
+        } else {
+            const channels = getLocalChannels();
+            if(channels) {
+                // 没有 token，但本地有 channels数据，就把localstorage的存储到redux
+                dispatch(saveUserChannels(channels))
+            } else {
+                // 如果没有token，且本地没有channels数据，则发请求获取基本的channels数据
+                const res = await request.get('/user/channels');
+                dispatch(saveUserChannels(res.data.channels));
+                // save to localstorage
+                setLocalChannels(res.data.channels)
+            }
+        }
     }
 }
 
--- geek-react-h5/src/utils/storage.js
+++ geek-react-h5/src/utils/storage.js
@@ -1,4 +1,5 @@
 const TOKEN_KEY = 'geek-itcast'
+const CHANNEL_KEY = 'geek-itcast-21-channels'
 
 // get token
 export const getTokenInfo = () => {
@@ -18,4 +19,21 @@ export const removeTokenInfo = (tokenInfo) => {
 // has token
 export const hasToken = () => {
     return !!getTokenInfo().token
+}
+
+/**
+ * 
+ * @param {*} channels 
+ */
+export const setLocalChannels = (channels) => {
+    localStorage.setItem(CHANNEL_KEY, JSON.stringify(channels))
+}
+
+// 获取本地频道数据，不要默认为空数组，因为如果没有数据，if判断出来空数组也是 true
+export const getLocalChannels = () => {
+    return JSON.parse(localStorage.getItem(CHANNEL_KEY))
+}
+
+export const removeLocalChannels = () => {
+    localStorage.removeItem(CHANNEL_KEY)
 }
