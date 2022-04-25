218a3d7b68962eb91d4ce061bf2536ca5520ac0b
JuanYuan <kitty.yuan525@gmail.com>
2022-04-25 10:23:35 +1000

    home page - get tabs info from redux

--- geek-react-h5/src/pages/Home/index.js
+++ geek-react-h5/src/pages/Home/index.js
@@ -1,33 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './index.module.scss'
 import Tabs from '@/components/Tabs'
+import Icon from '@/components/Icon'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserChannels } from '@/store/actions/home'
 
 export default function Home() {
-  const tabs = [
-    {
-      id: 1,
-      name: '频道1',
-    },
-    {
-      id: 2,
-      name: '频道2',
-    },
-    {
-      id: 3,
-      name: '频道3',
-    },
-    {
-      id: 4,
-      name: '频道4',
-    },
-    {
-      id: 5,
-      name: '频道5',
-    },
-  ]
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getUserChannels())
+  }, [dispatch])
+
+  const tabs = useSelector(state => state.home.userChannels)
   return (
     <div className={styles.root}>
       <Tabs tabs={tabs}></Tabs>
+      <div className="tabs-opration">
+        <Icon type="iconbtn_search"></Icon>
+        <Icon type="iconbtn_channel"></Icon>
+      </div>
     </div>
   )
 }
--- geek-react-h5/src/store/actions/login.js
+++ geek-react-h5/src/store/actions/login.js
@@ -1,4 +1,5 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
+
 import  { setTokenInfo, removeTokenInfo } from '@/utils/storage'
 
 export const sendCode = (mobile) => {
