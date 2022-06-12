75ecef00369e58fe0cac0beea110e41658a96b8e
JuanYuan <kitty.yuan525@gmail.com>
2022-06-12 22:30:01 +1000

    TS Retrofit - Improve Input component to accept any property

--- geek-react-h5/src/components/Input/index.tsx
+++ geek-react-h5/src/components/Input/index.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useRef } from 'react'
+import React, { ReactNode, useEffect, useRef } from 'react'
+import {  InputHTMLAttributes } from 'react-router/node_modules/@types/react'
 import styles from './index.module.scss'
 import classNames from 'classnames'
 
-type Props = {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   extra?: string
   onExtraClick?: () => void
   className?: string
   autoFocus?: boolean
+  type?: 'text' | 'password'
+  children?: ReactNode | '{}'
 }
+
 export default function Input({
   extra, 
   onExtraClick, 
