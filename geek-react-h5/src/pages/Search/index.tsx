c53636ff00ef27aaa38b3621f716ae1d16b3f520
JuanYuan <kitty.yuan525@gmail.com>
2022-06-19 21:18:56 +1000

    Search page -lodash debounce solved debounce

--- geek-react-h5/src/pages/Search/index.tsx
+++ geek-react-h5/src/pages/Search/index.tsx
@@ -4,27 +4,48 @@ import NavBar from '@/components/NavBar'
 import classNames from 'classnames'
 import { useHistory } from 'react-router'
 import styles from './index.module.scss'
+import debounce from 'lodash/debounce'
+import { DebouncedFunc } from 'lodash'
 
+let fetDate: DebouncedFunc<() => void>
 const Search = () => {
     const history = useHistory()
     const [keyword, setKeyword] = useState('')
-    const timeRef = useRef(-1)
 
-    useEffect(() => {
-        return () => {
-            clearTimeout(timeRef.current)
-        }
-    }, [])
-
+    // 防抖第二种方法： lodash来做：
+    // const fetchRef = useRef<DebouncedFunc<() => void>>(null)
+    if(!fetDate) {
+        fetDate = debounce(() => {
+            console.log('发送请求')
+        }, 500)
+    }
+    
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const text = e.target.value  //这样定时器里面就是最新的值,如果定时器里使用 keyword就会有闭包问题
         setKeyword(text)
-        clearTimeout(timeRef.current)
-        timeRef.current = window.setTimeout(() => {
-            console.log('text', text)
-        }, 500)
-        console.log('需要发送请求进行搜索')
+        fetDate()
     }
+
+    // 1. 防抖第一种方法：定时器做防抖 
+    // const timeRef = useRef(-1)
+    
+    // useEffect(() => {
+    //     return () => {
+    //         clearTimeout(timeRef.current)
+    //     }
+    // }, [])
+
+    // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const text = e.target.value  //这样定时器里面就是最新的值,如果定时器里使用 keyword就会有闭包问题
+    //     setKeyword(text)
+    //     clearTimeout(timeRef.current)
+    //     timeRef.current = window.setTimeout(() => {
+    //         console.log('text', text)
+    //     }, 500)
+    //     console.log('需要发送请求进行搜索')
+    // }
+
+    
     return (
         <div className={styles.root}>
             <NavBar
