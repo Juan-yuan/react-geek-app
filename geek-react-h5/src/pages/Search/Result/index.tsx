39160426230905e0972745fc128f6cd2144da396
JuanYuan <kitty.yuan525@gmail.com>
2022-06-26 20:12:15 +1000

    Search page - Use urlSearchParams to get data from url

--- geek-react-h5/src/pages/Search/Result/index.tsx
+++ geek-react-h5/src/pages/Search/Result/index.tsx
@@ -1,7 +1,12 @@
 import NavBar from '@/components/NavBar'
 import styles from './index.module.scss'
+import { useLocation } from 'react-router-dom'
 
 const SearchResult = () => {
+    const location = useLocation()
+    const search = new URLSearchParams(location.search)
+    const key = search.get('key')
+    console.log('location', key)
     return (
         <div className={styles.root}>
             <NavBar>搜索结果</NavBar>
