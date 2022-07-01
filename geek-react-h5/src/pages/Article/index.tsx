433e6867b6bb4962ea674deca0832576040219e5
JuanYuan <kitty.yuan525@gmail.com>
2022-07-01 21:41:08 +1000

    Article page - Render data into page

--- geek-react-h5/src/pages/Article/index.tsx
+++ geek-react-h5/src/pages/Article/index.tsx
@@ -1,10 +1,13 @@
 import Icon from '@/components/Icon'
 import NavBar from "@/components/NavBar"
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useParams } from 'react-router'
 import styles from './index.module.scss'
 import { getArticleDetail } from "@/store/actions/article"
+import { RootState } from '@/store'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
 
 const Article = () => {
     const history = useHistory()
@@ -13,6 +16,8 @@ const Article = () => {
     useEffect(() => {
         dispatch(getArticleDetail(id))
     }, [dispatch, id])
+
+    const { detail } = useSelector((state: RootState) => state.article)
 
     return (
         <div className={styles.root}>
@@ -36,26 +41,25 @@ const Article = () => {
                     <div className="wrapper">
                         <div className="article-wrapper">
                             <div className="header">
-                                <h1 className="title">{'测试文字 1234'}</h1>  
+                                <h1 className="title">{detail.title}</h1>  
 
                                 <div className="info">
-                                    <span>{'2020-10-10'}</span>
-                                    <span>{10}阅读</span>
-                                    <span>{10}评论</span>
+                                    <span>{detail.pubdate}</span>
+                                    <span>{detail.read_count}阅读</span>
+                                    <span>{detail.comm_count}评论</span>
                                 </div> 
 
                                 <div className="author">
-                                    <img src={''} alt="" />
-                                    <span className="name">{'张三'}</span>
-                                    <span className="follow">关注</span>
+                                    <img src={detail.aut_photo} alt="" />
+                                    <span className="name">{detail.aut_name}</span>
+                                    <span className={classNames('follow', { followed: detail.is_followed})}>{detail.is_followed ? '已关注' : '关注'}</span>
                                 </div>
                             </div>
                             <div className="content">
-                                <div className="content-html dg-html">
-                                    测试内容123
+                                <div className="content-html dg-html" dangerouslySetInnerHTML={{__html: detail.content}}>                                    
                                 </div>
                                 <div className="date">
-                                    发布文章时间：{'2020-10-10'}
+                                    发布文章时间：{dayjs(detail.pubdate).format('YYYY-MM-DD')}
                                 </div>
                             </div>
                         </div>                        
