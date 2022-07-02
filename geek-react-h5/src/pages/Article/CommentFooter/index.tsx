219763532f0cc0b0ebfccd72371808632ff52128
JuanYuan <kitty.yuan525@gmail.com>
2022-07-02 20:43:02 +1000

    Article comment page - Rendering comment processing

--- geek-react-h5/src/pages/Article/CommentFooter/index.tsx
+++ geek-react-h5/src/pages/Article/CommentFooter/index.tsx
@@ -1,7 +1,10 @@
 import Icon from '@/components/Icon'
+import { RootState } from '@/store'
+import { useSelector } from 'react-redux'
 import styles from './index.module.scss'
 
 const CommentFooter = () => {
+  const { detail } = useSelector((state: RootState) => state.article)
   return (
     <div className={styles.root}>
       <div className="input-btn">
@@ -12,18 +15,18 @@ const CommentFooter = () => {
           <div className="action-item">
             <Icon type="iconbtn_comment" />
             <p>评论</p>
-             <span className="bage">0</span>
+             <span className="bage">{detail.comm_count}</span>
           </div>
           <div className="action-item" >
             <Icon
-              type='iconbtn_like_sel'
+              type={detail.attitude === 1 ? 'iconbtn_like_sel' : 'iconbtn_like2'}
             />
             <p>点赞</p>
           </div>
         </>
 
       <div className="action-item">
-        <Icon type='iconbtn_collect_sel' />
+        <Icon type={detail.is_collected ? 'iconbtn_collect_sel' : 'iconbtn_collect'} />
         <p>收藏</p>
       </div>
       <div className="action-item">
